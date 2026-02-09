fixed dice computation, added inference on 5 folds and script for stat on soft values

soft-seg/evaluate_segs.py
@@ -15,12 +15,14 @@ import json
 import nibabel as nib
 from scipy.ndimage import zoom
 from tqdm import tqdm
+import pandas as pd
+from loguru import logger
+import matplotlib.pyplot as plt
 
 
 def dice_score(prediction, groundtruth, smooth=1.):
     numer = (prediction * groundtruth).sum()
     denor = (prediction + groundtruth).sum()
-    # loss = (2 * numer + self.smooth) / (denor + self.smooth)
     dice = (2 * numer + smooth) / (denor + smooth)
     return dice
 
@@ -33,24 +35,26 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    msd_path = args.msd
-    pred_folder = args.pred_folder
-    output_folder = args.o
+def main_dice_eval(msd_path, pred_folder, output_folder):
+
+    # We also want to save all results to log file
+    log_path = os.path.join(output_folder, "dice_analysis.log")
+    ## remove existing log file if it exists
+    if os.path.exists(log_path):
+        os.remove(log_path)
+    logger.add(log_path, format="{time} {level} {message}", level="INFO")
 
     # Load MSD json file
     with open(msd_path, "r") as f:
         msd_dict = json.load(f)
 
-    msd_data = msd_dict["test"]
+    msd_data = msd_dict["images"]
 
     # Get all soft segmentation in the pred_folder
     soft_seg_files = list(Path(pred_folder).rglob("*_lesion.nii.gz"))
     soft_seg_files = [str(f) for f in soft_seg_files if "/soft/" in str(f)]
 
-    dice_scores = {}
-    all_dices = []
+    dice_scores_df = pd.DataFrame(columns=["subject", "resamp_factor", "dice_at_0.5", "dice_at_1e-6"])
 
     for pred_img in tqdm(soft_seg_files):
         # img name
@@ -61,6 +65,9 @@ def main():
             if image_name in msd_image["image"]:
                 corresponding_img = msd_image
                 break
+
+        if corresponding_img is None:
+            return ValueError(f"No corresponding entry found in MSD json for image {image_name}")
 
         # Load ground truth
         gt_file = corresponding_img["label"]
@@ -74,31 +81,73 @@ def main():
         if soft_data.shape != gt_data.shape:
             zoom_factors = [gt_data.shape[i] / soft_data.shape[i] for i in range(3)]
             soft_data = zoom(soft_data, zoom_factors, order=1)  # Linear interpolation
+        
+        # Compute dice at 0.5
         ## Binarize soft segmentation
         soft_data_thresh = soft_data.copy()
-        soft_data_thresh[soft_data_thresh > 0] = 1
+        soft_data_thresh[soft_data_thresh > 0.5] = 1
+        soft_data_thresh[soft_data_thresh <= 0.5] = 0
+        dice_at_05 = dice_score(soft_data_thresh, gt_data)
+        
+        # Compute dice at 1e-6
+        soft_data_thresh = soft_data.copy()
+        soft_data_thresh[soft_data_thresh > 1e-6] = 1
+        soft_data_thresh[soft_data_thresh <= 1e-6] = 0
+        dice_at_1e_6 = dice_score(soft_data_thresh, gt_data)
 
-        # Compute Dice score
-        if dice_scores.get(image_name) is None:
-            dice_scores[image_name] = {}
-        dice = dice_score(soft_data_thresh, gt_data)
-        dice_scores[image_name][pred_img.split("/")[-1].replace(".nii.gz", "")] = dice
-        all_dices.append(dice)
+        # Update dice scores dataframe
+        resamp_factor = pred_img.split("/")[-1].split("_")[-2]
+        new_line = pd.DataFrame({"subject": [image_name], "resamp_factor": [resamp_factor], "dice_at_0.5": [dice_at_05], "dice_at_1e-6": [dice_at_1e_6]})
+        dice_scores_df = pd.concat([dice_scores_df, new_line], ignore_index=True)
 
-    # Compute average dice score
-    avg_dice = sum(all_dices) / len(all_dices)
-    dice_scores["avg"] = avg_dice
+    # Save dice scores dataframe to csv
+    dice_scores_df.to_csv(os.path.join(output_folder, "dice_scores.csv"), index=False)
+    logger.info(f"Saved {os.path.join(output_folder, 'dice_scores.csv')}")
 
-    # Save dice scores
-    os.makedirs(output_folder, exist_ok=True)
-    dice_output_path = os.path.join(output_folder, "dice_scores.json")
-    with open(dice_output_path, "w") as f:
-        json.dump(dice_scores, f, indent=4)
+    # Compute average dice score per image
+    ## At 0.5
+    img_avg_dice_05 = dice_scores_df.groupby("subject")["dice_at_0.5"].mean()
+    ## At 1e-6
+    img_avg_dice_1e_6 = dice_scores_df.groupby("subject")["dice_at_1e-6"].mean()
+    logger.info(f" ------------ Avg dice score per subject ------------ ")
+    logger.info(f"Average Dice at 0.5: {img_avg_dice_05.mean():.4f}")
+    logger.info(f"Average Dice at 1e-6: {img_avg_dice_1e_6.mean():.4f}")
 
-    # Print average dice score
-    avg_dice = dice_scores["avg"]
-    print(f"Average Dice score: {avg_dice}")
+    # Compute average dice score across all images
+    avg_dice_05 = dice_scores_df["dice_at_0.5"].mean()
+    avg_dice_1e_6 = dice_scores_df["dice_at_1e-6"].mean()
+    logger.info(f" ------------ Avg dice score across all subjects ------------ ")
+    logger.info(f"Average Dice at 0.5: {avg_dice_05:.4f}")
+    logger.info(f"Average Dice at 1e-6: {avg_dice_1e_6:.4f}")
+    
+    # Create a dict for resampling factors
+    resamp_factors = ['0.5x0.5x0.5', '0.5x0.5x1.0', '0.5x1.0x0.5', '1.0x0.5x0.5', '0.6x0.6x0.6', '0.6x0.6x1.0', '0.6x1.0x0.6',
+                      '1.0x0.6x0.6', '0.7x0.7x0.7', '0.7x0.7x1.0', '0.7x1.0x0.7', '1.0x0.7x0.7', '0.8x0.8x0.8', '0.8x0.8x1.0',
+                      '0.8x1.0x0.8', '1.0x0.8x0.8', '0.9x0.9x0.9', '0.9x0.9x1.0', '0.9x1.0x0.9', '1.0x0.9x0.9']
+    resamp_factors_dict = {resamp_factor: idx for idx, resamp_factor in enumerate(resamp_factors)}
+    dice_scores_df["resamp_factor"] = dice_scores_df["resamp_factor"].map(resamp_factors_dict)
+
+    # Plot dice score at 0.5 per resampling factor
+    dice_scores_df['resamp_factor'] = dice_scores_df['resamp_factor'].astype(str)
+    df_plot = dice_scores_df.groupby("resamp_factor")["dice_at_0.5"].mean().reset_index()
+    plt.plot(df_plot["resamp_factor"], df_plot["dice_at_0.5"], marker='o', linestyle='-', label='Dice at 0.5')
+    df_plot = dice_scores_df.groupby("resamp_factor")["dice_at_1e-6"].mean().reset_index()
+    plt.plot(df_plot["resamp_factor"], df_plot["dice_at_1e-6"], marker='o', linestyle='-', label='Dice at 1e-6')
+    plt.xlabel("Resampling Factor")
+    plt.ylabel("Dice Score")
+    plt.title("Dice Score per Resampling Factor")
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.legend()
+    plt.savefig(os.path.join(output_folder, "dice_score_per_resampling_factor.png"))
+    logger.info(f"Saved {os.path.join(output_folder, 'dice_score_per_resampling_factor.png')}")
 
 
 if __name__ == "__main__":
-    main()
+    # Parse arguments
+    args = parse_args()
+    msd_path = args.msd
+    pred_folder = args.pred_folder
+    output_folder = args.o
+
+    # Run main evaluation
+    main_dice_eval(msd_path, pred_folder, output_folder)

soft-seg/run_pred_without_sct.py
@@ -4,6 +4,7 @@ In this script, we run predictions without using SCT so that it is faster.
 Input:
     -i: Path to input folder containing images
     --path-model: Path to the nnUNet model to use for prediction
+    --five-folds: Whether to use 5-folds ensemble for prediction (instead of just fold 2 which is the best fold on the validation set)
     -o: Path to output folder to save predictions
 
 Author: Pierre-Louis Benveniste
@@ -29,6 +30,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Run lesion segmentation on all images in a folder.")
     parser.add_argument("-i", "--input_folder", type=str, required=True, help="Path to input folder containing images")
     parser.add_argument("--path-model", type=str, required=True, help="Path to the nnUNet model to use for prediction")
+    parser.add_argument("--five-folds", action="store_true", help="Whether to use 5-folds ensemble for prediction (instead of just fold 2 which is the best fold on the validation set)")
     parser.add_argument("-o", "--output_folder", type=str, required=True, help="Path to output folder to save predictions")
     return parser.parse_args()
 
@@ -47,10 +49,12 @@ def load_trainer_class_if_available(path_model):
     return None
 
 
-def initialize_predictor(path_model):
+def initialize_predictor(path_model, five_folds=False):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # We load the model
     folds = [2]
+    if five_folds:
+        folds = [0, 1, 2, 3, 4]
     predictor = nnUNetPredictor(
         tile_step_size=0.5,  # changing it from 0.5 to 0.9 makes inference faster
         use_gaussian=True,  # applies gaussian noise and gaussian blur
@@ -71,14 +75,14 @@ def initialize_predictor(path_model):
     return predictor
 
 
-def main_run_pred(input_folder, output_folder, path_model):
+def main_run_pred(input_folder, output_folder, path_model, five_folds=False):
     # Create output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
     
     input_images = list(Path(input_folder).rglob("*.nii.gz"))
     input_images = [str(img) for img in input_images]
 
-    predictor = initialize_predictor(path_model)
+    predictor = initialize_predictor(path_model, five_folds=five_folds)
 
     # Now for each image, we run the prediction
     for img in tqdm(input_images, desc="Running lesion segmentation"):
@@ -94,6 +98,7 @@ def main_run_pred(input_folder, output_folder, path_model):
         # Run nnUNet prediction
         data = img_in.data.transpose([2, 1, 0])
         data = np.expand_dims(data, axis=0).astype(np.float32)
+        
         pred = predictor.predict_single_npy_array(
             input_image=data,
             # The spacings also have to be reversed to match nnUNet's conventions.
@@ -149,5 +154,6 @@ if __name__ == "__main__":
     input_folder = args.input_folder
     output_folder = args.output_folder
     path_model = args.path_model
+    five_folds = args.five_folds
 
-    main_run_pred(input_folder, output_folder, path_model)
+    main_run_pred(input_folder, output_folder, path_model, five_folds)