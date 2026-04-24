fe6b7c206a3adaebcc9a1e6dc4d03a268b3b9b31
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2026-04-24 12:21:56 -0400

    updated the script to work with new batch of WMn data

--- nnunet/convert_reorient_with_wmn_data.py
+++ nnunet/convert_reorient_with_wmn_data.py
@@ -14,6 +14,8 @@ import shutil
 import argparse
 from pathlib import Path
 import nibabel as nib
+import json
+from tqdm import tqdm
 
 
 def parse_args():
@@ -38,20 +40,38 @@ def main():
     list_derivatives = list(Path(wmn_data).rglob("*label-lesion_seg.nii.gz"))
     list_derivatives = [str(path) for path in list_derivatives]
 
-    print(f"Found {len(list_derivatives)} WMn images in the WMn dataset")
+    # Get all WMn images
+    list_images = list(Path(wmn_data).rglob("*WMn.nii.gz"))
+    list_images = [str(path) for path in list_images]
+
+    image_pairs = {}
+    # For each image we keep only the derivative with the highest rater number (e.g. desc-rater4_label-lesion_seg.nii.gz should be kept over desc-rater1_label-lesion_seg.nii.gz)
+    for image in tqdm(list_images):
+        image_name = image.split('/')[-1].replace(".nii.gz", "")
+        corresponding_derivative = [derivative for derivative in list_derivatives if image_name in derivative]
+        # Only the derivative with the highest rater number should be kept (e.g. desc-rater4_label-lesion_seg.nii.gz should be kept over desc-rater1_label-lesion_seg.nii.gz)
+        derivatives_to_remove = sorted(corresponding_derivative, key=lambda x: int(x.split('rater')[1].split('_')[0]), reverse=True)
+        image_pairs[image] = derivatives_to_remove[0]
+
+    # Remove variable list_derivatives
+    del list_derivatives
+    del list_images
+
+    print(f"Found {len(image_pairs)} WMn images in the WMn dataset")
 
     # remove images where sub-XXX greater or equal to 099
     if not args.with_hc:
-        list_derivatives = [path for path in list_derivatives if int(path.split('/')[-1].split('_')[0].replace("sub-", "")) < 99]
-
-    print(f"Found {len(list_derivatives)} WMn images to convert and add to nnUNet dataset")
+        image_pairs = {image: derivative for image, derivative in image_pairs.items() if int(image.split('/')[-1].split('_')[0].split('sub-')[1]) < 99}
+        
+    print(f"Found {len(image_pairs)} WMn images to convert and add to nnUNet dataset")
+    
     # Initialize values of each data split
     count_Tr = 3926
     count_Ts = 433
 
     list_subjects = []
-    for path in list_derivatives:
-        subject_id = path.split('/')[-1].split('_')[0]
+    for sub in image_pairs.keys():
+        subject_id = sub.split('/')[-1].split('_')[0]
         list_subjects.append(subject_id)
 
     # Split the subjects into train and test sets 85/15
@@ -60,6 +80,14 @@ def main():
 
     # Load the conversion dict in the existing nnUNet dataset if it exists, otherwise create an empty dict
     conversion_dict_path = os.path.join(nnunet_data, "conversion_dict.json")
+    # Load the conversion dict
+    with open(conversion_dict_path, "r") as f:
+        conversion_dict = json.load(f)
+
+    # Load the dataset.json file in the existing nnUNet dataset
+    dataset_json_path = os.path.join(nnunet_data, "dataset.json")
+    with open(dataset_json_path, "r") as f:
+        dataset_json = json.load(f)
 
     # Build output folders
     path_out_imagesTr = os.path.join(nnunet_data, 'imagesTr')
@@ -72,11 +100,12 @@ def main():
     os.makedirs(path_out_labelsTs, exist_ok=True)
 
     # Iterate over the derivatives and move them to the nnunet dataset
-    for seg_file in list_derivatives:
-        subject_id = seg_file.split('/')[-1].split('_')[0]
-        print(f"Processing subject {subject_id} with path {seg_file}")
-        # Find corresponding image file
-        image_file = seg_file.replace("_label-lesion_seg.nii.gz", ".nii.gz").replace("derivatives/labels/", "")
+    for image_file in tqdm(image_pairs):
+        subject_id = image_file.split('/')[-1].split('_')[0]
+        print(f"Processing subject {subject_id} with path {image_file}")
+        # Find corresponding seg file
+        seg_file = image_pairs[image_file]
+    
         if subject_id in list_subjects_train:
             count_Tr +=1
             image_file_nnunet = os.path.join(path_out_imagesTr,f'Dataset903_msLesionAgnostic_{count_Tr:03d}_0000.nii.gz')
@@ -93,30 +122,71 @@ def main():
             # Then we reorient the label to RPI
             assert os.system(f"sct_image -i {label_file_nnunet} -setorient RPI -o {label_file_nnunet}") ==0
 
+            # For each label fils, we reorient them to the same orientation as the image using sct_register_multimodal -identity 1
+            assert os.system(f"sct_register_multimodal -i {str(label_file_nnunet)} -d {str(image_file_nnunet)} -identity 1 -o {str(label_file_nnunet)} -owarp file_to_delete.nii.gz -owarpinv file_to_delete_2.nii.gz ") ==0
+            # Remove the other useless files
+            assert os.system("rm file_to_delete.nii.gz file_to_delete_2.nii.gz") ==0
+            other_file_to_remove = str(label_file_nnunet).replace('.nii.gz', '_inv.nii.gz')
+            assert os.system(f"rm {other_file_to_remove}") ==0
+
+            # Then we binarize the label
+            assert os.system(f"sct_maths -i {str(label_file_nnunet)} -bin 0.5 -o {str(label_file_nnunet)}") ==0
+
+            # Add the file to the dataset.json file
+            dataset_json['training'].append({
+                "image": image_file_nnunet,
+                "label": label_file_nnunet
+            })
+
+        else:
+            count_Ts +=1
+            image_file_nnunet = os.path.join(path_out_imagesTs,f'Dataset903_msLesionAgnostic_{count_Ts:03d}_0000.nii.gz')
+            label_file_nnunet = os.path.join(path_out_labelsTs,f'Dataset903_msLesionAgnostic_{count_Ts:03d}.nii.gz')
+
+            # Reorient the image
+            assert os.system(f"sct_image -i {image_file} -setorient RPI -o {image_file_nnunet}") ==0
+
+            # Binarize the label and save it to the adequate path
+            label = nib.load(seg_file).get_fdata()
+            label[label > 0] = 1
+            label = nib.Nifti1Image(label, nib.load(seg_file).affine)
+            nib.save(label, label_file_nnunet)
+            # Then we reorient the label to RPI
+            assert os.system(f"sct_image -i {label_file_nnunet} -setorient RPI -o {label_file_nnunet}") ==0
+
+            # For each label fils, we reorient them to the same orientation as the image using sct_register_multimodal -identity 1
+            assert os.system(f"sct_register_multimodal -i {str(label_file_nnunet)} -d {str(image_file_nnunet)} -identity 1 -o {str(label_file_nnunet)} -owarp file_to_delete.nii.gz -owarpinv file_to_delete_2.nii.gz ") ==0
+            # Remove the other useless files
+            assert os.system("rm file_to_delete.nii.gz file_to_delete_2.nii.gz") ==0
+            other_file_to_remove = str(label_file_nnunet).replace('.nii.gz', '_inv.nii.gz')
+            assert os.system(f"rm {other_file_to_remove}") ==0
+
+            # Then we binarize the label
+            assert os.system(f"sct_maths -i {str(label_file_nnunet)} -bin 0.5 -o {str(label_file_nnunet)}") ==0
+
+             # Add the file to the dataset.json file
+            dataset_json['test'].append({
+                "image": image_file_nnunet,
+                "label": label_file_nnunet
+            })
+
         # Update the conversion dict
-        conversion_dict[str(os.path.abspath(img_dict['image']))] = image_file_nnunet
-        conversion_dict[str(os.path.abspath(img_dict['label']))] = label_file_nnunet
+        conversion_dict[image_file] = image_file_nnunet
+        conversion_dict[seg_file] = label_file_nnunet
+    
+    # Update the dataset.json files count values
+    dataset_json['numTraining'] = count_Tr
+    dataset_json['numTest'] = count_Ts
 
-        # For each label fils, we reorient them to the same orientation as the image using sct_register_multimodal -identity 1
-        assert os.system(f"sct_register_multimodal -i {str(label_file_nnunet)} -d {str(image_file_nnunet)} -identity 1 -o {str(label_file_nnunet)} -owarp file_to_delete.nii.gz -owarpinv file_to_delete_2.nii.gz ") ==0
-        # Remove the other useless files
-        assert os.system("rm file_to_delete.nii.gz file_to_delete_2.nii.gz") ==0
-        other_file_to_remove = str(label_file_nnunet).replace('.nii.gz', '_inv.nii.gz')
-        assert os.system(f"rm {other_file_to_remove}") ==0
+    # Save the updated dataset.json file
+    with open(dataset_json_path, "w") as f:
+        json.dump(dataset_json, f, indent=4)
 
-        # Then we binarize the label
-        assert os.system(f"sct_maths -i {str(label_file_nnunet)} -bin 0.5 -o {str(label_file_nnunet)}") ==0
+    # Save the updated conversion dict
+    with open(conversion_dict_path, "w") as f:
+        json.dump(conversion_dict, f, indent=4)
 
-        break
-
-    # TODO: do the same for the test set and test the above (issue with the conversion dict)
-
-            
-        # elif subject_id in list_subjects_test:
-
-    # 
-
-
+    return None
 
     
 if __name__ == "__main__":
